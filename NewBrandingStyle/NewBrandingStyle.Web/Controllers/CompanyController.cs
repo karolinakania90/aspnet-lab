dce3957ea4f48f3c8b9b3f8b9e0e8ce567c04ba6
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2020-10-22 19:03:21 +0200

    update lab1

--- NewBrandingStyle/NewBrandingStyle.Web/Controllers/CompanyController.cs
+++ NewBrandingStyle/NewBrandingStyle.Web/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,23 @@ namespace NewBrandingStyle.Web.Controllers
 {
     public class CompanyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
         }
     }
 }

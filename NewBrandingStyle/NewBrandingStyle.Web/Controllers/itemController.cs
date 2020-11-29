4cda8dfdf6ccd401cce17bf13b4f67cbb5392661
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2020-11-29 11:41:55 +0100

    lab 3

--- NewBrandingStyle/NewBrandingStyle.Web/Controllers/itemController.cs
+++ NewBrandingStyle/NewBrandingStyle.Web/Controllers/itemController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+using NewBrandingStyle.Web.Entities;
 using NewBrandingStyle.Web.Models;
 
 namespace NewBrandingStyle.Web.Controllers
@@ -7,14 +9,34 @@ namespace NewBrandingStyle.Web.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
+        private ExchangesDbContext dbContext;
+
+        public ItemController(ExchangesDbContext exchangesDbContext) {
+            this.dbContext = exchangesDbContext;
+        }
 
         [HttpPost("new")]
         public AddNewItemResponse Post(ItemModel model)
         {
+
+            var itemEntity = new ItemEntity() {
+                Description = model.Description,
+                IsVisible = model.IsVisible,
+                Name = model.Name
+            };
+
+            dbContext.Add(itemEntity);
+            dbContext.SaveChanges();
+
             return new AddNewItemResponse()
             {
                 Success = !string.IsNullOrEmpty(model.Name) && !string.IsNullOrEmpty(model.Description)
             };
+        }
+
+        [HttpGet("saved")]
+        public JsonResult Saved() {
+            return new JsonResult(dbContext.ItemEntities);
         }
     }
 }
---
+++ NewBrandingStyle/NewBrandingStyle.Web/Database/ExchangesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewBrandingStyle.Web.Entities;
+
+namespace NewBrandingStyle.Web.Database {
+    public class ExchangesDbContext : DbContext {
+
+        public ExchangesDbContext(DbContextOptions options) : base(options) {
+
+        }
+
+        public DbSet<ItemEntity> ItemEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            // fluent configuration ...
+        }
+
+    }
+}

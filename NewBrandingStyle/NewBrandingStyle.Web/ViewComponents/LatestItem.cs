569b0f60bdac09090f1e52e2e968722e7ed009fb
Karolina Kania <karolina.kania@microsoft.wsei.edu.pl>
2020-12-12 15:24:53 +0100

    lab 4

---
+++ NewBrandingStyle/NewBrandingStyle.Web/ViewComponents/LatestItem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext exchangesDbContext;
+        public LatestItem(ExchangesDbContext exchangesDbContext)
+        {
+            this.exchangesDbContext = exchangesDbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = this.exchangesDbContext.ItemEntities.OrderByDescending(c => c.Id).FirstOrDefault();
+
+            return View("Index", latestItem);
+
+        }
+    }
+}
